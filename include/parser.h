a138203a2f583ea2cfdcbb91afd7c3ea7ffa041e
cRYP70n-13 <otmane.kimdil@gmail.com>
2021-04-12 17:46:11 +0000

    The Parsing part

--- include/parser.h
+++ include/parser.h
@@ -5,13 +5,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct is_quote {
+typedef struct s_quote {
 	short int 	single_quote; // 1 or 0
 	short int 	double_quote; // 1 or 0
 	size_t 		num_quotes; // is it odd or even
-}				t_is_quote;
+}				t_quote;
 
-typedef enum	t_bool
+typedef struct s_cmd
+{
+	char *cmd;
+	char *arg;
+	char separator;
+}				t_cmd;
+
+typedef enum	s_bool
 {
 	false = 0,
 	true = !false  // This is just for the compiler so that he takes care of false value based on the data type
@@ -20,5 +27,6 @@ typedef enum	t_bool
 int 		check_for_double_quote(char *line);
 int 		is_it_cmd(char *line);
 int 		parse_special_chars(char *line);
+t_cmd		*parse_line(char *line, t_cmd *commands);
 
 #endif
